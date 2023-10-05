1a2b7f40fbb601aa0cdec5c33849358c34978739
Sindhura <sindhura0925@gmail.com>
2023-10-05 14:28:54 -0500

    ec2 v1

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+region  = "us-east-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-}
+ resource "aws_instance" "demo-server" {
+  ami           = "ami-0bb4c991fa89d4b9b"
+  instance_type = "t2.micro"
+  key_name       = "dpp"
+ }
+
+ 
+
